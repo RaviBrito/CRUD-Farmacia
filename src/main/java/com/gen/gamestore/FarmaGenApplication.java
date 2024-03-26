2aa599f73278aa4387a586ebf0f4db6cbcc3fa4e
Ravi Brito <ravibriton@gmail.com>
2024-03-26 10:10:43 -0300

    configurando projeto

---
+++ src/main/java/com/gen/gamestore/FarmaGenApplication.java
@@ -0,0 +1,13 @@
+package com.gen.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaGenApplication.class, args);
+	}
+
+}
